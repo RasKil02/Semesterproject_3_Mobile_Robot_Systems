90f139b34062ab686fbbd0123e4039358e6e26f6
Rasmus <rasmus_kildeberg@outlook.dk>
2025-11-26 12:04:14 +0100

    gap

--- Main/test_signal_processing.py
+++ Main/test_signal_processing.py
@@ -22,7 +22,7 @@ def readCommand():
     )
 
     # --- Stabilizer ---
-    stabilizer = DigitStabilizer(hold_ms=20, miss_ms=20, gap_ms=85)
+    stabilizer = DigitStabilizer(hold_ms=20, miss_ms=20, gap_ms=75)
 
     # --- Audio sampler (streaming) ---
     sampler = AudioSampler(fs=args.fs)
