c680675b5089c38b3d2363903c0373986c088c9a
Rasmus <rasmus_kildeberg@outlook.dk>
2025-12-07 14:55:59 +0100

    AAARGHH

--- SignalProc/DTMFDetector.py
+++ SignalProc/DTMFDetector.py
@@ -405,6 +405,11 @@ class DTMFDetector:
             
             timer += block_ms
 
+            digits.append(out)
+            print("Detected digits so far:", "".join(digits))
+            print(f"Timer: {timer} ms")
+
+
             if len(digits) < 8 and timer >= 6000.0:
                 print("Timeout while collecting digits → resetting")
                 digits.clear()
