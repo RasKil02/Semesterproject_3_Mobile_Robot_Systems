f37a353e9d4b98eeded971b0856e8f4045dad8db
Rasmus <rasmus_kildeberg@outlook.dk>
2025-12-07 14:25:59 +0100

    fuck

--- SignalProc/DTMFDetector.py
+++ SignalProc/DTMFDetector.py
@@ -372,7 +372,7 @@ class DTMFDetector:
             t_ms += block_ms
 
             if len(digits) >= 0:
-                print("t_ms:", t_ms, "block_ms:", block_ms)
+                print("t_ms_resey:", t_ms_reset, "block_ms:", block_ms)
                 t_ms_reset += block_ms
 
             # plotting
