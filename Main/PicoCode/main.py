12aa1531f552204cd1154718272b0f6394ad1b92
Nikolai <nikolaidahljensen@gmail.com>
2025-12-08 11:27:08 +0100

    main til pico

--- Main/PicoCode/main.py
+++ Main/PicoCode/main.py
@@ -2,6 +2,13 @@ import time
 import sys
 from machine import Pin, PWM
 
+
+def run_motor(duration):
+    end = time.ticks_add(time.ticks_ms(), int(duration*1000))
+    while time.ticks_diff(end, time.ticks_ms()) > 0:
+        time.sleep(0.01)  # keep USB alive
+
+
 def supplyMotor(supplyID: int, duty: int = int(0.5 * 65535), duration: float = 10.0):
     """
     supplyID: motor number (0–3)
@@ -23,13 +30,11 @@ def supplyMotor(supplyID: int, duty: int = int(0.5 * 65535), duration: float = 1
     print("Activating supply motor", supplyID)
     pwm.duty_u16(duty)
 
-    time.sleep(duration)
+    run_motor(duration)
 
     # Turn off PWM
     pwm.duty_u16(0)
     pwm.deinit()
-
-    pin_obj = Pin(pin_num, Pin.OUT)
     pin_obj.value(0)
 
     print("Deactivating supply motor", supplyID)
@@ -44,8 +49,8 @@ def send_ack(supplyID):
 
 def main():
     # Debug LED on pin 22
-    pin19 = Pin(19, Pin.OUT)
-    pin19.value(1)
+    pin20 = Pin(20, Pin.OUT)
+    pin20.value(1)
 
     print("Pico ready. Listening on USB serial...")
 
