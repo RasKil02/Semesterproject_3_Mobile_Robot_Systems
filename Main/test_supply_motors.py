f0e24971329aa53c575741e83b5052fe87b96c5e
Nikolai <nikolaidahljensen@gmail.com>
2025-12-08 10:16:45 +0100

    fuck

--- Main/test_supply_motors.py
+++ Main/test_supply_motors.py
@@ -1,10 +1,17 @@
 from DriveSystem.PicoMotorController import PicoMotorController
+import time
 
 picosender = PicoMotorController()  
 
 def main():
     print("Sending supply ID 2 to Pico...")
     picosender.send_supply_id(2)
+
+    time.sleep(4)
+
+    print("Sending supply ID 1 to Pico...")
+    picosender.send_supply_id(1)
+
 
 if __name__ == "__main__":
     main()
