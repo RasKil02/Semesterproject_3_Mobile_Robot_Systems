32ef7349bc8c144a806a5187bb39d1a525927a22
Nikolai <nikolaidahljensen@gmail.com>
2025-12-08 12:32:58 +0100

    fuckery 2

--- Main/test_supply_motors.py
+++ Main/test_supply_motors.py
@@ -4,19 +4,19 @@ import time
 def main():
 
     pico = PicoMotorController()
+    
     print("Sending command 2...")
     ack = pico.send_supply_id(2)
     print("Pico replied:", ack)
-    pico.close()
 
-    time.sleep(4)
+    time.sleep(5)
 
-    pico = PicoMotorController()
     print("Sending command 1...")
     ack = pico.send_supply_id(1)
     print("Pico replied:", ack)
-    pico.close()
 
+    pico.close()   # <---- Only close AFTER all commands
+                    # DO NOT re-open in the same program
 
 if __name__ == "__main__":
     main()
