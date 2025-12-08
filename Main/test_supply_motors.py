7c2986b1071e62dc7085b4aa2d4c5904d3eba230
Nikolai <nikolaidahljensen@gmail.com>
2025-12-08 09:56:28 +0100

    added test_supply_motor main script

---
+++ Main/test_supply_motors.py
@@ -0,0 +1,14 @@
+from DriveSystem.PicoMotorController import PicoMotorController
+
+picosender = PicoMotorController()  
+
+def main():
+    print("Sending supply ID 1 to Pico...")
+    picosender.send_supply_id(1)
+
+    print("Sending supply ID 2 to Pico...")
+    picosender.send_supply_id(2)
+
+if __name__ == "__main__":
+    main()
+    picosender.close()
